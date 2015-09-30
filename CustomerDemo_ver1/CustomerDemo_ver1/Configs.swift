3e27eba2d461ac6459ca42eec202e4e283ee88a3
Thanhphn <thanhpham1@ThanhPhams-Mini.mecorp.local>
2015-09-30 10:43:56 +0700

    structure code

---
+++ CustomerDemo_ver1/CustomerDemo_ver1/Configs.swift
@@ -0,0 +1,15 @@
+//
+//  Configs.swift
+//  CustomerDemo_ver1
+//
+//  Created by Thanhphn on 9/30/15.
+//  Copyright (c) 2015 Thanhphn. All rights reserved.
+//
+
+import UIKit
+
+class Configs: NSObject {
+    
+    let url = ""
+   
+}
